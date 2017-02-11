4dbeb6f53477d39dd0fa0933add09ec4ddd1aa45
Justin Swanson <justin.c.swanson@gmail.com>
2017-02-11 13:34:40 -0600

    Checkout hook now retrieves and passes shas properly. Added Libgit2sharp

--- FishingWithGit/HookManager.cs
+++ FishingWithGit/HookManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -32,7 +33,7 @@ namespace FishingWithGit
             switch (type)
             {
                 case CommandType.checkout:
-                    return CheckoutHooks(args);
+                    return CheckoutHooks(args, index);
                 case CommandType.rebase:
                     return RebaseHooks(args);
                 default:
@@ -62,18 +63,36 @@ namespace FishingWithGit
             return null;
         }
 
-        public HookPair CheckoutHooks(string[] args)
+        public HookPair CheckoutHooks(string[] args, int commandIndex)
         {
+            if (args.Length <= commandIndex + 1)
+            {
+                throw new ArgumentException("Cannot run checkout hooks, as args are invald.  No content was found after checkout command.");
+            }
+            
+            string curSha, targetSha;
+            using (var repo = new Repository(Directory.GetCurrentDirectory()))
+            {
+                curSha = repo.Head.Tip.Sha;
+                targetSha = repo.Branches[args[commandIndex + 1]].Tip.Sha;
+            }
+
+            var newArgs = new string[]
+            {
+                curSha,
+                targetSha
+            };
+
             return new HookPair()
             {
                 PreCommand = () =>
                 {
-                    FireHook(HookType.Pre_Checkout, HookLocation.InRepo);
-                    FireHook(HookType.Pre_Checkout, HookLocation.Normal);
+                    FireHook(HookType.Pre_Checkout, HookLocation.InRepo, newArgs);
+                    FireHook(HookType.Pre_Checkout, HookLocation.Normal, newArgs);
                 },
                 PostCommand = () =>
                 {
-                    FireHook(HookType.Post_Checkout, HookLocation.InRepo);
+                    FireHook(HookType.Post_Checkout, HookLocation.InRepo, newArgs);
                     // Normal already exists.
                 }
             };
@@ -105,11 +124,11 @@ namespace FishingWithGit
             }
             path += "/hooks";
             FileInfo file = new FileInfo($"{path}/{commandType.HookName()}");
-            wrapper.WriteLine("Looking for hook file " + file.FullName, writeToConsole: true);
+            wrapper.WriteLine("Looking for hook file " + file.FullName);
             if (!file.Exists) return;
 
             wrapper.WriteLine($"Firing {hookType} {commandType.HookName()}", writeToConsole: true);
-            
+
             var processInfo = new ProcessStartInfo("cmd.exe", "/c " + file.FullName + " " + string.Join(" ", args));
             processInfo.CreateNoWindow = true;
             processInfo.UseShellExecute = false;
