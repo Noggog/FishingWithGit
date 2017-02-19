33b82404cd2cd4029c084afdb3d0f6ed1ff0482b
Justin Swanson <justin.c.swanson@gmail.com>
2017-02-19 14:54:49 -0600

    CheckoutHook now handles when branch is only on remote repo

--- FishingWithGit/Hooks/CheckoutHooks.cs
+++ FishingWithGit/Hooks/CheckoutHooks.cs
@@ -12,30 +12,10 @@ namespace FishingWithGit
     {
         string[] newArgs;
 
-        private CheckoutHooks(BaseWrapper wrapper, string[] args, int commandIndex, string targetBranchName)
+        private CheckoutHooks(BaseWrapper wrapper, string[] args, int commandIndex)
             : base(wrapper)
         {
-            string curSha, targetSha;
-            using (var repo = new Repository(Directory.GetCurrentDirectory()))
-            {
-                curSha = repo.Head.Tip.Sha;
-                var targetBranch = repo.Branches[targetBranchName];
-                if (targetBranch == null)
-                {
-                    throw new ArgumentException($"Could not locate branch named {targetBranchName} in repo {repo.Info.Path}");
-                }
-                if (targetBranch.Tip == null)
-                {
-                    throw new ArgumentException($"Branch named {targetBranchName} did not point to a commit in repo {repo.Info.Path}.");
-                }
-                targetSha = targetBranch.Tip.Sha;
-            }
-
-            newArgs = new string[]
-            {
-                curSha,
-                targetSha
-            };
+            this.newArgs = RetrieveNewArgs(args, commandIndex);
         }
 
         public static HookSet Factory(BaseWrapper wrapper, string[] args, int commandIndex)
@@ -48,14 +28,58 @@ namespace FishingWithGit
             }
             else
             {
-                var nameIndex = CommonFunctions.Skip(args, commandIndex + 1, "-b");
-                if (nameIndex >= args.Length)
-                {
-                    throw new ArgumentException("Cannot run checkout hooks, as args are invald.  No branch name was found.");
-                }
-                return new CheckoutHooks(wrapper, args, commandIndex, args[nameIndex]);
+                return new CheckoutHooks(wrapper, args, commandIndex);
             }
         }
+
+        private string[] RetrieveNewArgs(string[] args, int commandIndex)
+        {
+            string curSha, targetSha;
+            var argsList = args.ToList();
+            var trackIndex = argsList.IndexOf("--track");
+            using (var repo = new Repository(Directory.GetCurrentDirectory()))
+            {
+                curSha = repo.Head.Tip.Sha;
+                if (trackIndex == -1)
+                { // Just checking out a local branch
+                    var nameIndex = CommonFunctions.Skip(args, commandIndex + 1, "-b");
+                    if (nameIndex >= args.Length)
+                    {
+                        throw new ArgumentException("Cannot run checkout hooks, as args are invald.  No branch name was found.");
+                    }
+                    targetSha = GetTargetSha(repo, args[nameIndex]);
+                }
+                else
+                { // Checking out a remote branch
+                    if (args.Length <= trackIndex + 1)
+                    {
+                        throw new ArgumentException($"Could not locate target remote branch name.");
+                    }
+                    
+                    targetSha = GetTargetSha(repo, args[trackIndex + 1]);
+                }
+            }
+
+            return new string[]
+            {
+                curSha,
+                targetSha
+            };
+        }
+
+        private string GetTargetSha(Repository repo, string targetBranchName)
+        {
+            var targetBranch = repo.Branches[targetBranchName];
+            if (targetBranch == null)
+            {
+                throw new ArgumentException($"Could not locate local branch named {targetBranchName} in repo {repo.Info.Path}");
+            }
+            if (targetBranch.Tip == null)
+            {
+                throw new ArgumentException($"Branch named {targetBranchName} did not point to a commit in repo {repo.Info.Path}.");
+            }
+            return targetBranch.Tip.Sha;
+        } 
 
         public override int PreCommand()
         {
