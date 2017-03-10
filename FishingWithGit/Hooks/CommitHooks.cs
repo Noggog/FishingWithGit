7ccc0cdc8812ed6519bf5b9a85f711ede6663f3f
Justin Swanson <justin.c.swanson@gmail.com>
2017-03-09 19:41:24 -0600

    Added amend pre-commit hook parameter

--- FishingWithGit/Hooks/CommitHooks.cs
+++ FishingWithGit/Hooks/CommitHooks.cs
@@ -8,21 +8,25 @@ namespace FishingWithGit
 {
     public class CommitHooks : HookSet
     {
-        private CommitHooks(BaseWrapper wrapper)
+        bool amend;
+
+        private CommitHooks(BaseWrapper wrapper, List<string> args)
             : base(wrapper)
         {
+            this.amend = args.Contains("--amend");
         }
 
         public static HookSet Factory(BaseWrapper wrapper, List<string> args, int commandIndex)
         {
-            return new CommitHooks(wrapper);
+            return new CommitHooks(wrapper, args);
         }
 
         public override Task<int> PreCommand()
         {
+            string[] args = amend ? new string[] { "--amend" } : new string[0];
             return CommonFunctions.RunCommands(
-                () => this.Wrapper.FireAllHooks(HookType.Pre_Commit, HookLocation.InRepo),
-                () => this.Wrapper.FireUnnaturalHooks(HookType.Pre_Commit, HookLocation.Normal));
+                () => this.Wrapper.FireAllHooks(HookType.Pre_Commit, HookLocation.InRepo, args),
+                () => this.Wrapper.FireUnnaturalHooks(HookType.Pre_Commit, HookLocation.Normal, args));
         }
 
         public override Task<int> PostCommand()
