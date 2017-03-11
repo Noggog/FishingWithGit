f2273d7405605a338efaa83f0794aae1e3df5421
Justin Swanson <justin.c.swanson@gmail.com>
2017-03-10 21:43:05 -0600

    Some Rebase arg passing

--- FishingWithGit/Hooks/RebaseHooks.cs
+++ FishingWithGit/Hooks/RebaseHooks.cs
@@ -8,28 +8,47 @@ namespace FishingWithGit
 {
     public class RebaseHooks : HookSet
     {
-        private RebaseHooks(BaseWrapper wrapper) 
+        string[] args;
+
+        private RebaseHooks(BaseWrapper wrapper, string[] args) 
             : base(wrapper)
         {
+            this.args = args;
         }
 
         public static HookSet Factory(BaseWrapper wrapper, List<string> args, int commandIndex)
         {
-            return new RebaseHooks(wrapper);
+            if (args.Count <= commandIndex + 1)
+            {
+                throw new ArgumentException("No target branch argument provided.");
+            }
+
+            var targetBranch = args[commandIndex + 1];
+            var curBranch = args.Count > commandIndex + 2 ? args[commandIndex + 2] : null;
+            var l = new List<string>();
+            l.Add(targetBranch);
+            if (curBranch != null)
+            {
+                l.Add(curBranch);
+            }
+
+            return new RebaseHooks(
+                wrapper,
+                l.ToArray());
         }
 
         public override Task<int> PreCommand()
         {
             return CommonFunctions.RunCommands(
-                () => this.Wrapper.FireAllHooks(HookType.Pre_Rebase, HookLocation.InRepo),
-                () => this.Wrapper.FireUnnaturalHooks(HookType.Pre_Rebase, HookLocation.Normal));
+                () => this.Wrapper.FireAllHooks(HookType.Pre_Rebase, HookLocation.InRepo, args),
+                () => this.Wrapper.FireUnnaturalHooks(HookType.Pre_Rebase, HookLocation.Normal, args));
         }
 
         public override Task<int> PostCommand()
         {
             return CommonFunctions.RunCommands(
-                () => this.Wrapper.FireAllHooks(HookType.Post_Rebase, HookLocation.InRepo),
-                () => this.Wrapper.FireAllHooks(HookType.Post_Rebase, HookLocation.Normal));
+                () => this.Wrapper.FireAllHooks(HookType.Post_Rebase, HookLocation.InRepo, args),
+                () => this.Wrapper.FireAllHooks(HookType.Post_Rebase, HookLocation.Normal, args));
         }
     }
 }
