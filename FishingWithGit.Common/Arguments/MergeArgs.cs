141517280e1d5ace65a46163b1ac8cf139ef16fe
Justin Swanson <justin.c.swanson@gmail.com>
2017-03-12 23:06:09 -0500

    MergeArgs

---
+++ FishingWithGit.Common/Arguments/MergeArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingWithGit
+{
+    public class MergeArgs : IEnumerable<string>
+    {
+        public string TargetBranch;
+
+        public MergeArgs(string[] args, int startIndex = 0)
+        {
+            if (args.Length < 1 + startIndex)
+            {
+                throw new ArgumentException("Target branch argument was not provided.");
+            }
+
+            this.TargetBranch = args[startIndex];
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            yield return TargetBranch;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
--- FishingWithGit/Hooks/MergeHooks.cs
+++ FishingWithGit/Hooks/MergeHooks.cs
@@ -8,33 +8,33 @@ namespace FishingWithGit
 {
     public class MergeHooks : HookSet
     {
-        private string targetBranch;
-        private string[] args;
+        MergeArgs args;
 
-        public MergeHooks(BaseWrapper wrapper, List<string> args)
+        public MergeHooks(BaseWrapper wrapper, List<string> args, int commandIndex)
             : base(wrapper)
         {
-            this.targetBranch = args[0];
-            this.args = new string[] { targetBranch };
+            this.args = new MergeArgs(
+                args.ToArray(),
+                commandIndex + 1);
         }
 
         public static HookSet Factory(BaseWrapper wrapper, List<string> args, int commandIndex)
         {
-            return new MergeHooks(wrapper, args);
+            return new MergeHooks(wrapper, args, commandIndex);
         }
 
         public override Task<int> PreCommand()
         {
             return CommonFunctions.RunCommands(
-                () => this.Wrapper.FireAllHooks(HookType.Pre_Commit, HookLocation.InRepo, args),
-                () => this.Wrapper.FireUnnaturalHooks(HookType.Pre_Commit, HookLocation.Normal, args));
+                () => this.Wrapper.FireAllHooks(HookType.Pre_Commit, HookLocation.InRepo, args.ToArray()),
+                () => this.Wrapper.FireUnnaturalHooks(HookType.Pre_Commit, HookLocation.Normal, args.ToArray()));
         }
 
         public override Task<int> PostCommand()
         {
             return CommonFunctions.RunCommands(
-                () => this.Wrapper.FireAllHooks(HookType.Post_Commit, HookLocation.InRepo, args),
-                () => this.Wrapper.FireUnnaturalHooks(HookType.Post_Commit, HookLocation.Normal, args));
+                () => this.Wrapper.FireAllHooks(HookType.Post_Commit, HookLocation.InRepo, args.ToArray()),
+                () => this.Wrapper.FireUnnaturalHooks(HookType.Post_Commit, HookLocation.Normal, args.ToArray()));
         }
     }
 }
